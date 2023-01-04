83feefd0b7b4c7f59aa22bf532099dbfb7fe0f06
Ibrahim Haruna <harsoftng@gmail.com>
2023-01-04 03:27:32 +0100

    Setup CORS

--- config/cors.php
+++ config/cors.php
@@ -17,9 +17,9 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:1760', 'http://localhost:1761'],
 
     'allowed_origins_patterns' => [],
 
